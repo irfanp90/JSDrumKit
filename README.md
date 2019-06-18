b7ddddf5dc0f4a7bb6d7e524facdb4799fc77ade
irfanp90 <irfanp90@gmail.com>
2019-06-17 19:55:54 -0500

    js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 <h1>JavaScript Drum Kit</h1>
 
 <p>If you hit one of the letters displayed on the screen, a sound will be played</p>
+
+<h3>DO NOT FORGET TO TURN SOUNDS ON!!!</h3>
---
+++ music.js
@@ -0,0 +1,21 @@
+
+    function playSound(e){
+    // console.log(e.keyCode)
+       const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+       // console.log(audio)
+       const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+       // console.log(key)
+       if (!audio) return;
+       audio.currentTime = 0;
+       audio.play()
+       key.classList.add("playing")
+   }
+   
+   function removeTransition(event){
+       console.log(event);
+       if(event.propertyName !== "transform") return;
+       this.classList.remove("playing");
+   }       
+   const keys = document.querySelectorAll(".key");
+   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+   window.addEventListener("keydown", playSound);
